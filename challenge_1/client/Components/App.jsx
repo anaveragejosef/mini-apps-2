10dddfb03966efc76ea4a0ded0c57c6a8aeacf67
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-07-29 15:16:58 -0700

    Adds Functionality for Search Bar
    - Builds Search Bar component that updates state on change and submits
    an axios request on submit
    - Imports axios into the project
    - Updates App to contain the update and submit functions, as well as,
    build out more of the render

--- challenge_1/client/Components/App.jsx
+++ challenge_1/client/Components/App.jsx
@@ -1,9 +1,60 @@
 import React from 'react';
+import axios from 'axios';
+import SearchBar from './SearchBar.jsx';
+import DisplayResult from './DisplayResult.jsx';
 
-const App = (props) => {
-  return (
-    <h1>Testing</h1>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchString: '',
+      searchResults: []
+    };
+    this.updateSearch = this.updateSearch.bind(this);
+    this.submitSearch = this.submitSearch.bind(this);
+    this.searchResultsRender = this.searchResultsRender.bind(this);
+  }
+
+  updateSearch (event) {
+    this.setState({
+      searchString: event.target.value
+    })
+  }
+
+  submitSearch (event) {
+    event.preventDefault();
+    axios.get(`/events?q=${this.state.searchString}`)
+      .then(response => {
+        this.setState({
+          searchResults: response,
+          searchString: ''
+        });
+      })
+      .catch(error => console.log('ERROR - ', error));
+  }
+
+  searchResultsRender() {
+    if (this.searchResults.length === 0) {
+      return <p>Time for a new search!</p>
+    } else {
+      return (
+        this.searchResults.map(record => <DisplayResult record={record} />)
+      );
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <div>
+          <SearchBar updateSearch={this.updateSearch} submitSearch={this.submitSearch} />
+        </div>
+        <div>
+          {searchResultsRender()}
+        </div>
+      </div>
+    );
+  };
 }
 
 export default App;
