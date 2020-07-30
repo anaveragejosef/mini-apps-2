52e11010dde91d26e1a14c44e18434d2ad4e4ffd
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-07-30 13:07:17 -0700

    Builds Chart using Coin Desk Data
    - Makes small correction to index.jsx file
    - Builds a chart on the app.jsx page using Chart.js
    - Populates chart with the closing price in EUR over the past few
    months and returns it to the page
    - This was built using Hooks and the useEffect option

--- challenge_2/client/Components/app.jsx
+++ challenge_2/client/Components/app.jsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Chart from 'chart.js';
+import axios from 'axios';
 
 const App = (props) => {
+  const [chartRef, setRef] = useState(React.createRef());
+
+  useEffect (() => {
+    axios.get('http://localhost:3000/api/historical-data')
+      .then(response => {
+        let dates = Object.keys(response.data);
+        let prices = dates.map(date => Number(response.data[date].toFixed(2)));
+        const btcChart = chartRef.current.getContext("2d");
+        new Chart(btcChart, {
+          type: 'line',
+          data: {
+            labels: dates,
+            datasets: [
+              {
+                label: "BTC Closing Price (EUR)",
+                data: prices
+              }
+            ]
+          }
+        });
+      })
+      .catch(error => `Error getting hisorical data ${error}`);
+  });
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <canvas
+        id='BTCChart'
+        ref={chartRef}
+        style={{width: "600"}, {height: "400"}}
+      />
+      <br />
+      <p>Powered by <a href="https://www.coindesk.com/price/bitcoin">CoinDesk</a></p>
+    </div>
   );
 }
 
--- challenge_2/client/index.jsx
+++ challenge_2/client/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/app.jsx';
 
-ReactDOM.render(<App />, getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
