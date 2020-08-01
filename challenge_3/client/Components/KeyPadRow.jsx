4689014e4aa3df0cad09c3dc8be26980c58f486e
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-08-01 08:52:01 -0700

    Updates Button Component for Styling
    Updates the button component to only return a Button, which allows for
    the keypad to render correctly.

--- challenge_3/client/Components/KeyPadRow.jsx
+++ challenge_3/client/Components/KeyPadRow.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 const KeyPadRow = ({ number, updateScore }) => {
   return (
-    <div className='keypad-row'>
-      <button value={number} onClick={updateScore}>{number}</button>
-    </div>
+    <button value={number} onClick={updateScore}>{number}</button>
   )
 }
 
