9f7cabfc6f584b910cca302a0d34b1229035bbf4
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-08-01 15:06:49 -0700

    Adds Initial Logic for Bowling Scores
    - Adds the initial logic in the bowling scores to keep track of the
    players score for the frame and for the game
    - Also adds some minor changes to the flex box stylings

--- challenge_3/client/Components/BowlingAlley.jsx
+++ challenge_3/client/Components/BowlingAlley.jsx
@@ -9,31 +9,74 @@ class BowlingAlley extends React.Component {
       currentRoll: 1,
       currentScore: 0,
       hitPins: 0,
+      frameOne: 0,
+      allRounds: []
     }
     this.updateScore = this.updateScore.bind(this);
   }
 
   updateScore(event) {
-    // Num of pins = event.target.value
-    // Increment score by that amount
-    // Increment hit pins by that amount
-      // If hit pins === 10 && roll === 1
-        // Strike
-        // Increment round
-        // return
-      // if hit pins >= 10 && roll === 2
-        // Spare
-    // If roll + 1 > 2
-      // Round over, increment current round by 1
-      // Reset roll to 1
-    // Else
-      // Increment roll to 2
+    const rounds = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
+    let totalPins = Number(event.target.value);
+    const currRound = rounds[this.state.currentRound];
+    if (totalPins === 10 && this.state.currentRoll === 1) {
+      // Strike
+      this.setState({
+        currentRound: this.state.currentRound + 1,
+        currentScore: this.state.currentScore + 10,
+        allRounds: allRounds.concat([10, 0, 10])
+      });
+    } else if (totalPins + this.state.hitPins >= 10 && this.state.currentRoll === 2) {
+      // Spare
+      this.setState({
+        currentRound: this.state.currentRound + 1,
+        currentScore: this.state.currentScore + 10,
+        currentRoll: 1,
+        hitPins: 0,
+        allRounds: allRounds.concat([this.state.frameOne, (10 - this.state.frameOne), 10])
+      });
+    } else if (this.state.currentRoll === 2) {
+      let prevRound = rounds[this.state.currentRound - 1]
+      // Prior round was strike
+      if (this.state[prevRound][0] === 10) {
+        this.setState({
+          [prevRound]: [10, 0, (10 + this.state.FrameOne + totalPins)],
+          currentScore: this.state.currentScore + this.state.FrameOne + totalPins
+        })
+      } else if (this.state[prevRound][2] === 10) { // Prior round was spare
+        let spareRound = this.state[prevRound].slice();
+        spareRound.splice(2, 1, (this.state.frameOne + 10));
+        this.setState({
+          [prevRound]: spareRound,
+          currentScore: this.state.currentScore + this.state.FrameOne
+        })
+      }
+      this.setState({
+        currentRound: this.state.currentRound + 1,
+        currentScore: this.state.currentScore + this.state.frameOne + totalPins,
+        hitPins: 0,
+        currentRoll: 1,
+        allRounds: allRounds.concat([this.state.frameOne, totalPins, (this.state.frameOne + totalPins)])
+      });
+    } else {
+      this.setState({
+        currentRoll: this.state.currentRoll + 1,
+        frameOne: totalPins,
+        hitPins: totalPins
+      });
+    }
   }
 
   render() {
     return (
-      <div style={{display: 'flex'}, {justifyContent: 'center'}, {alignItems: 'center'}}>
-        <KeyPad updateScore={this.updateScore} />
+      <div style={{display: 'flex'}, {alignItems: 'center'}, {justifyContent: 'center'}}>
+        <div>
+          <h1>Welcome to Bowling</h1>
+        </div>
+        <div>
+          <p>How many pins did you hit?</p>
+          <KeyPad updateScore={this.updateScore} />
+        </div>
       </div>
     );
   }
--- challenge_3/client/Components/KeyPad.jsx
+++ challenge_3/client/Components/KeyPad.jsx
@@ -3,17 +3,17 @@ import KeyPadRow from './KeyPadRow.jsx';
 
 const KeyPad = ({ updateScore }) => {
   return (
-    <div style={{display: 'flex'}, {flexDirection: 'column'}}>
-      <div style={{display: 'flex'}, {flexDirection: 'row'}}>
+    <div style={{flexDirection: 'column'}}>
+      <div style={{flexDirection: 'row'}}>
         {[0, 1, 2].map(num => <KeyPadRow number={num} updateScore={updateScore} key={num} />)}
       </div>
-      <div style={{display: 'flex'}, {flexDirection: 'row'}}>
+      <div style={{flexDirection: 'row'}}>
         {[3, 4, 5].map(num => <KeyPadRow number={num} updateScore={updateScore} key={num} />)}
       </div>
-      <div style={{display: 'flex'}, {flexDirection: 'row'}}>
+      <div style={{flexDirection: 'row'}}>
         {[6, 7, 8].map(num => <KeyPadRow number={num} updateScore={updateScore} key={num} />)}
       </div>
-      <div style={{display: 'flex'}, {flexDirection: 'row'}}>
+      <div style={{flexDirection: 'row'}}>
         {[9, 10].map(num => <KeyPadRow number={num} updateScore={updateScore} key={num} />)}
       </div>
     </div>
